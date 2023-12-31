a2a4a09487cefdbefa59b04594e82a5d8b18a58a
Quang Le <112357287+qle21@users.noreply.github.com>
2023-12-31 00:25:43 -0800

    Add files via upload

---
+++ extra.c
@@ -0,0 +1,1 @@
+double definitely = 0;
---
+++ hw1.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+char *and_the = "abc";
+extern double definitely;
+double* indices  = &definitely ;
+
+const int matter(){
+static int symbols ;
+ return symbols  ;
+}
+
+static void important(){
+static int for_these= 123; 
+static int symbols = 68 ;
+}
+
+char types ='abc';
+char the[2];
+
+const char scopes[15];  
+static int *isnt[2]; 
+static int order = 1 ;
+
+
+int main(int argc,char* argv[]){
+ int symbols = 0 ; 
+indices = NULL;
+ important();
+  puts("qle21\n");
+  return 0;
+}
